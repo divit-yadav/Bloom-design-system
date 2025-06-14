641d486d68d05b63e3335985dff68ad9c6635361
Beenu Yadav <beenuyadav@Beenus-MacBook-Air.local>
2025-06-14 12:25:10 +0530

    update

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -3,12 +3,15 @@
 import { addons } from '@storybook/manager-api';
 import { create } from '@storybook/theming';
 
+const homepage = 'https://divit-yadav.github.io/my-design-system'; // Your GitHub Pages homepage
+
 const myTheme = create({
   base: 'light',
   appBg: '#f0f3f4', // Sets the background color of the UI
   barBg: '#f0f3f4', // Sets the sidebar and top bar color
-  brandUrl: 'https://divit-yadav.github.io/my-design-system',
-  brandImage: 'logo.svg', // Uncomment and set if you have a logo
+  brandTitle: 'Bloom Design System',
+  brandUrl: homepage,
+  brandImage: `${homepage}/logo.svg`, // Absolute path for GitHub Pages
 });
 
 addons.setConfig({
