aed1508638c5964d2cf55e6ec580168b9926f40f
Beenu Yadav <beenuyadav@Beenus-MacBook-Air.local>
2025-06-14 12:09:03 +0530

    update

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -7,7 +7,6 @@ const myTheme = create({
   base: 'light',
   appBg: '#f0f3f4', // Sets the background color of the UI
   barBg: '#f0f3f4', // Sets the sidebar and top bar color
-  brandTitle: 'Bloom Design System',
   brandUrl: 'https://divit-yadav.github.io/my-design-system',
   brandImage: 'logo.svg', // Uncomment and set if you have a logo
 });
