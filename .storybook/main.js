bdb87530bb8451e6ec006f7b37af345a5265278e
Beenu Yadav <beenuyadav@Beenus-MacBook-Air.local>
2025-07-03 10:34:06 +0530

    update

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,9 +5,9 @@ const config = {
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-docs"
   ],
   "framework": {
     "name": "@storybook/react-webpack5",
@@ -16,29 +16,32 @@ const config = {
   "staticDirs": [
     "../public"
   ],
+  features: {
+    reactRefresh: false,
+  },
   // Set the base path for GitHub Pages deployment
   base: '/Bloom-design-system/',
-  webpackFinal: async (config) => {
-    // Ensure the publicPath is set correctly for GitHub Pages deployment
-    // This prepends '/Bloom-design-system/' to all asset URLs in production builds.
-    config.output.publicPath = process.env.NODE_ENV === 'production' ? '/Bloom-design-system/' : '/';
+  // webpackFinal: async (config) => {
+  //   // Ensure the publicPath is set correctly for GitHub Pages deployment
+  //   // This prepends '/Bloom-design-system/' to all asset URLs in production builds.
+  //   config.output.publicPath = process.env.NODE_ENV === 'production' ? '/Bloom-design-system/' : '/';
 
-    // Find and modify the HtmlWebpackPlugin to adjust CSP
-    const HtmlWebpackPlugin = config.plugins.find(
-      (p) => p.constructor.name === 'HtmlWebpackPlugin'
-    );
+  //   // Find and modify the HtmlWebpackPlugin to adjust CSP
+  //   const HtmlWebpackPlugin = config.plugins.find(
+  //     (p) => p.constructor.name === 'HtmlWebpackPlugin'
+  //   );
 
-    if (HtmlWebpackPlugin) {
-      HtmlWebpackPlugin.options.meta = {
-        ...HtmlWebpackPlugin.options.meta,
-        'Content-Security-Policy': {
-          'http-equiv': 'Content-Security-Policy',
-          'content': "default-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; worker-src 'self' blob:;"
-        }
-      };
-    }
+  //   if (HtmlWebpackPlugin) {
+  //     HtmlWebpackPlugin.options.meta = {
+  //       ...HtmlWebpackPlugin.options.meta,
+  //       'Content-Security-Policy': {
+  //         'http-equiv': 'Content-Security-Policy',
+  //         'content': "default-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; worker-src 'self' blob:;"
+  //       }
+  //     };
+  //   }
     
-    return config;
-  },
+  //   return config;
+  // },
 };
 export default config;
